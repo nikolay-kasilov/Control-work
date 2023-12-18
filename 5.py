6f0c9754d20b0d31f7e9ecb8b8793e8b99044576
Your Name <you@example.com>
2023-12-18 21:50:36 +0300

    new

---
+++ 5.py
@@ -0,0 +1,19 @@
+# class Cat :
+#     def __int__(self,name):
+#         self.name = name
+#     def meow(self):
+#         print(f'{self.name}:мяу')
+# cat = Cat('Барсик')
+# cat.meow()
+# cat2 = Cat('Мурзик')
+# print(id(cat))
+# print(id(cat2))
+
+# class student:
+#     def __int__(self,full_name , age ,course):
+#         self.full_name= full_name
+#         self.age = age
+#         self.course = course
+#     def  info(self):
+#         return (f'{self.full_name}:{self.age}лет ,{self.course}курс)
+

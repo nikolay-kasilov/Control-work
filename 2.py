392d0c771cf8c027c2b039c901f792357c84b39b
n_k <pekolian-dengi@yandex.ru>
2023-12-19 22:48:54 +0300

    commit_2.py

--- 2.py
+++ 2.py
@@ -5,28 +5,21 @@
 (большими буквами)
 Учитывая окончания, например  дожде -> ДОЖДЕ
 """
-import re
 
 
-def process_file(input_file: str, output_file: str) -> None:
-    """
-    Читает содержимое указанного файла, заменяет ключевые слова на верхний регистр
-    с сохранением окончаний и записывает измененный текст в новый файл.
 
-    nput_file: Имя исходного файла
-    output_file: Имя файла, в который будет записан измененный текст
-    """
-    with open(input_file, 'r', encoding='utf-8') as file:
-        text = file.read()
-        # Используем регулярное выражение для замены ключевых слов на верхний регистр с сохранением окончаний
-        modified_text = re.sub(r'\b(дожд[ьяюем]?)\b', lambda x: x.group(1).upper(), text)
-
-    with open(output_file, 'w', encoding='utf-8') as file:
-        file.write(modified_text)
-
-
-input_file = '2.txt'
-output_file = '2_rain.txt'
-process_file(input_file, output_file)
-
-
+with open('2.txt','r',encoding="utf-8")   as f:
+    text =[
+        line.split()
+        for line in f.readlines()
+    ]
+new_text =[]
+for line in text:
+    new_line =[]
+    for word in line:
+        if 'дожд' in  word.lower():
+            new_line.append(word.upper())
+        else:
+            new_line.append(word)
+    new_text.append(' '.join(new_line))
+print('\n'.join(new_text))

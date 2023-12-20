bb26c0da9f9bfac64317c35954b3c04e829a16ed
Your Name <you@example.com>
2023-12-20 20:12:57 +0300

    дополнение

--- 3.py
+++ 3.py
@@ -24,29 +24,12 @@
 Чтобы проверить работу можно использовать функцию ниже
 
 """
-def distribute_production(equally: int, production: int) -> int:
-    """
-    Генератор, который делит количество продукции максимально поровну на склады.
-
-    equally: Количество складов
-    production: Общее количество продукции
-    yield: Количество продукции для каждого склада
-    """
-    base_amount = production // equally  # Базовое количество продукции для каждого склада
-    remainder = production % equally  # Остаток после равномерного распределения
-
-    for _ in range(equally):
-        if remainder > 0:
-            yield base_amount + 1  # Добавляем к базовому значению по одной единице, пока есть остаток
-            remainder -= 1
-        else:
-            yield base_amount
-
-# Пример использования
-total_production = 5
-number_of_warehouses = 3
-
-for amount in distribute_production(number_of_warehouses, total_production):
-    print(amount)
-
-
+def solution (storages ,count):
+    for rest_storages in range(storages,0,-1):
+        c = count // rest_storages
+        count -= c
+        yield c
+def check (func, a,b):
+    print(list(func(a,b)))
+    print(sum(list(func(a,b))))
+check(solution , 5,97)
---
+++ main.py
@@ -0,0 +1,5 @@
+from classes import Quiz
+
+if __name__ == "main":
+    quiz =Quiz()
+    quiz.game()

1fe47a511b18d4495c71da6b2da9117f24f0fd15
n_k <pekolian-dengi@yandex.ru>
2023-12-23 20:03:47 +0300

    изменение 2.py

---
+++ 2.py
@@ -0,0 +1,29 @@
+"""
+Напишите скрипт, который прочитает файл 2.txt
+И создаст новый файл 2_raint.txt
+И точно так же запишет тот же текст, заменив все слова дождь на ДОЖДЬ
+(большими буквами)
+Учитывая окончания, например  дожде -> ДОЖДЕ
+"""
+
+
+
+with open('2.txt','r',encoding="utf-8")   as f:
+    text =[
+        line.split()
+        for line in f.readlines()
+    ]
+new_text =[]
+for line in text:
+    new_line =[]
+    for word in line:
+        if 'дожд' in  word.lower():
+            new_line.append(word.upper())
+        else:
+            new_line.append(word)
+    new_text.append(' '.join(new_line))
+
+print('\n'.join(new_text))
+
+with open('2_raint.txt','w',encoding="utf-8")   as f:
+    f.write('\n'.join(new_text) )

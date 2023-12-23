13ded4b5b2e29a33a735c1a6de01063dcbec95f3
nikolay-kasilov <147438143+nikolay-kasilov@users.noreply.github.com>
2023-12-23 19:51:58 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -5,49 +5,17 @@
 """
 
 """Всем привет -> Жцйс фхнжйч -> Всем привет"""  # Придумать что-то интереснее шифра Цезаря
+encode_text ="".join(
+    [
+        (chr(ord(letter)+4))
+        for letter in "Привет всем!"
+    ]
+)
+decode_text = "".join(
+    (chr(ord(letter)-4))
+    for letter in encode_text
+)
+print(decode_text)
+print(encode_text)
 
-
-def encrypt_text(text: str, key: dict) -> str:
-    """
-    Шифрует текст с использованием алгоритма полиалфавитной замены.
-
-    text: Исходная строка для шифрования
-    key: Словарь, представляющий ключ шифрования (пример: {'а': 'г', 'б': 'д', ...})
-    return: Зашифрованная строка
-    """
-    encrypted_text = ""
-    for char in text:
-        if char in key:
-            encrypted_text += key[char]
-        else:
-            encrypted_text += char
-    return encrypted_text
-
-
-def decrypt_text(text: str, key: dict) -> str:
-    """
-    Расшифровывает текст, зашифрованный с использованием алгоритма полиалфавитной замены.
-
-    text: Зашифрованная строка
-    key: Словарь, представляющий ключ шифрования
-    return: Расшифрованная строка
-    """
-    decrypted_text = ""
-    reversed_key = {v: k for k, v in key.items()}
-    for char in text:
-        if char in reversed_key:
-            decrypted_text += reversed_key[char]
-        else:
-            decrypted_text += char
-    return decrypted_text
-
-
-# Пример использования
-original_message = "я сделал контрольную"
-encryption_key = {'п': 'т', 'р': 'у', 'и': 'ф', 'в': 'ж', 'е': 'з', 'т': 'ю', ' ': ' '}  # Пример ключа шифрования
-
-encrypted_message = encrypt_text(original_message, encryption_key)
-print("Зашифрованное сообщение:", encrypted_message)
-
-decrypted_message = decrypt_text(encrypted_message, encryption_key)
-print("Расшифрованное сообщение:", decrypted_message)
+ 
